ef066c51b46bc9f81ab739cb9130408c5146ad04
Nicholas Kincaid & Josh Jones <pair+nbkincaid.z3ntech@devbootcamp.com>
2015-08-13 15:47:25 -0500

    Commit before merge

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,11 +1,15 @@
 post '/users' do
 
   if request.xhr?
+
+    p params
+
     @user = User.find_or_create_by(email: params["wc"]);
 
     @user.update_attributes(name:params["Ld"], photo_link:params["zt"])
 
     if @user.valid?
+      # session[:id] = @user.id
       content_type :json
       @user.to_json
     else
@@ -15,5 +19,10 @@ post '/users' do
 
   end
 
+end
 
+
+get "/users/:id" do
+  @user = User.find(params[:id])
+  erb :"/users/show"
 end
