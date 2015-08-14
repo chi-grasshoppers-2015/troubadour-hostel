04f23de8dc008002605fb5fbb65e35ebdcefd997
Nicholas Kincaid & Josh Jones <pair+nbkincaid.z3ntech@devbootcamp.com>
2015-08-14 14:25:35 -0500

    add changes to log out

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
   $(".pure-menu-list").on("click", ".log-out", function(event) {
     event.preventDefault();
-    console.log("we are conncted.")
+    console.log("we are connected.")
+
+    var cookies = $.cookie();
+    for(var cookie in cookies) {
+      $.removeCookie(cookie);
+    }
 
     gapi.auth.signOut();
 
-    $.ajax({
-      url: "/sessions",
-      method: "delete"
-    }).done( function() {
+    console.log("we are connected again.")
+
+    // $.ajax({
+    //   url: "/sessions",
+    //   method: "delete"
+    // }).done( function(response) {
       window.location.replace("https://th-grasshoppers.herokuapp.com");
-    })
+    // })
+
+    console.log("we should be done.")
   })
 
 });
