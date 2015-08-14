532a712e7c5e41af49f8ffb9ac310154029fb627
Nicholas Kincaid & Josh Jones <pair+nbkincaid.z3ntech@devbootcamp.com>
2015-08-14 12:56:21 -0500

    cleaned up google.js file

--- public/js/google.js
+++ public/js/google.js
@@ -1,17 +1,7 @@
 function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-  console.log("hey it's friday")
-  // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
   var token = {idToken: id_token}
-
-  console.log(id_token);
 
   var request = $.ajax({
                         method: "POST",
@@ -22,11 +12,6 @@ function onSignIn(googleUser) {
 
   request.done(function(response){
     window.location.replace("https://th-grasshoppers.herokuapp.com");
-    // console.log(response);
-    // $('body').append('<h1>' + response.name + '</h1>')
-    // $('body').append('<h2>' + response.email + '</h2>')
-    // $('body').append("<img src='"+ response.photo_link +"'>")
-
   });
 
 };
