cf9a17c2ecfaf414f6f70a0f2d130c48803f9a2c
Nicholas Kincaid & Josh Jones <pair+nbkincaid.z3ntech@devbootcamp.com>
2015-08-14 12:58:23 -0500

    added google logout

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $(".pure-menu-list").on("click", ".log-out", function(event) {
     event.preventDefault();
     console.log("we are conncted.")
+
+    gapi.auth.signOut();
+
     $.ajax({
       url: "/sessions",
       method: "delete"
